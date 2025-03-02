51207e7b7f3bd6fab72071f3b62ee522f70ca4a8
Ryan Da Costa <ryan.l.dacosta@gmail.com>
2025-03-02 07:15:07 +0530

    Updated open graph route

--- src/app/open-graph/image/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET(request: Request) {
-    const searchParams = new URL(request.url).searchParams
-    console.log(searchParams)
-   
-    return Response.json(searchParams.has('test') ? '/assets/home-image.svg' : '/logo.svg')
-  }
