eb7606e12bc30b289540abaee67d7ca592f20077
Ryan Da Costa <ryan.l.dacosta@gmail.com>
2025-03-02 08:11:38 +0530

    downgraded next js version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,12 @@ import MainLayout from "@/components/layout/MainLayout";
 export const metadata: Metadata = {
   title: "Globetrotter - Online Quiz",
   description: "An online quiz app to test your knowledge",
-  // openGraph: {
-  //   images: '/logo.svg',
-  //   title: 'Globetrotter - Explore the World Through Trivia',
-  //   description: 'Expand your global knowledge with our fun and engaging trivia app. Learn interesting facts about cities and countries while challenging your friends.'
-  // }
+  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL),
+  openGraph: {
+    images: '/logo.svg',
+    title: 'Globetrotter - Explore the World Through Trivia',
+    description: 'Expand your global knowledge with our fun and engaging trivia app. Learn interesting facts about cities and countries while challenging your friends.'
+  }
 };
 
 const theme: ThemeConfig = {
