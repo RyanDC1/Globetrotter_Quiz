e42991699266e61004de2d4cd3a1eb1f28587fea
Ryan Da Costa <ryan.l.dacosta@gmail.com>
2025-03-02 08:56:28 +0530

    updated open graph images

---
+++ src/app/invites/whatsapp/[id]/page.tsx
@@ -0,0 +1,44 @@
+import { getChallenge } from "@/actions/challengeActions";
+import { InviteDetails } from "@/components/invite";
+import { Metadata } from "next";
+
+interface Request {
+    params: Promise<{ id: string }>
+}
+
+type Props = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+    { searchParams }: Props
+): Promise<Metadata> {
+    // read route params
+    const test = searchParams?.['test']
+
+    return {
+        title: "Globetrotter - Online Quiz",
+        description: "An online quiz app to test your knowledge",
+        metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL),
+        openGraph: {
+            images: '/og-image-whatsapp.png',
+            title: 'Globetrotter - Explore the World Through Trivia',
+            description: 'Expand your global knowledge with our fun and engaging trivia app. Learn interesting facts about cities and countries while challenging your friends.'
+        }
+    }
+}
+
+
+export default async function Invite(request: Request) {
+
+    const { id } = await request.params
+
+    const challengerDetails = await getChallenge(id as string)
+
+    return (
+        <InviteDetails
+            {...challengerDetails}
+        />
+    );
+}
